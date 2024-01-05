6e9e0c8007296866fafdf42bf45cb50280bd8ac1
Adharsh <118920074+AdharshKokkula@users.noreply.github.com>
2024-01-05 23:40:21 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+let nameEl = document.getElementById("name");
+let nameErrMsgEl = document.getElementById("nameErrMsg");
+let emailEl = document.getElementById("email");
+let emailErrMsgEl = document.getElementById("emailErrMsg");
+
+nameEl.addEventListener("blur", function(event) {
+    if (event.target.value === "") {
+        nameErrMsgEl.textContent = "Required*";
+    } else {
+        nameErrMsgEl.textContent = "";
+    }
+});
+emailEl.addEventListener("blur", function(event) {
+    if (event.target.value === "") {
+        emailErrMsgEl.textContent = "Required*";
+    } else {
+        emailErrMsgEl.textContent = "";
+    }
+});
+
+
+let subscribeForm = document.getElementById("subscribeForm");
+subscribeForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+});
